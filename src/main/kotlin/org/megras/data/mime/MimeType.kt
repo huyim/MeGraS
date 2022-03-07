a9986f6b31e9e10f6e26b2f4ebc490fd31307769
Luca Rossetto <lucaro@lucaro.ch>
2022-03-07 21:17:44 +0100

    Added MediaType grouping

--- src/main/kotlin/org/megras/data/mime/MimeType.kt
+++ src/main/kotlin/org/megras/data/mime/MimeType.kt
@@ -19,11 +19,25 @@ enum class MimeType(val mimeString: String) {
     JPEG_I("image/jpeg"),
     PNG("image/png"),
     SVG("image/svg+xml"),
-    TIFF("image/tiff");
+    TIFF("image/tiff"),
+
+    //audio
+    AAC("audio/x-aac"),
+    ADP("audio/adpcm"),
+    AIF("audio/x-aiff"),
+    AU("audio/basic"),
+    MPEG_A("audio/mpeg"),
+    MIDI("audio/midi"),
+    MP4_A("audio/mp4"),
+    OGG("audio/ogg"),
+    WAV("audio/x-wav"),
+    WAX("audio/x-ms-wax"),
+    WMA("audio/x-ms-wma");
+
 
 
     companion object {
-        private val extensionMap = mapOf(
+        val extensionMap = mapOf(
             "css" to CSS,
             "csv" to CSV,
             "htm" to HTML,
@@ -43,7 +57,33 @@ enum class MimeType(val mimeString: String) {
             "svg" to SVG,
             "svgz" to SVG,
             "tif" to TIFF,
-            "tiff" to TIFF
+            "tiff" to TIFF,
+
+            "aac" to AAC,
+            "adp" to ADP,
+            "aif" to AIF,
+            "aifc" to AIF,
+            "aiff" to AIF,
+            "au" to AU,
+            "snd" to AU,
+            "m2a" to MPEG_A,
+            "m3a" to MPEG_A,
+            "mp2" to MPEG_A,
+            "mp2a" to MPEG_A,
+            "mp3" to MPEG_A,
+            "mpga" to MPEG_A,
+            "kar" to MIDI,
+            "mid" to MIDI,
+            "midi" to MIDI,
+            "rmi" to MIDI,
+            "mp4a" to MP4_A,
+            "oga" to OGG,
+            "ogg" to OGG,
+            "spx" to OGG,
+            "wav" to WAV,
+            "wax" to WAX,
+            "wma" to WMA
+
         )
 
         fun mimeType(fileExtension: String) = extensionMap[fileExtension.lowercase()] ?: OCTET_STREAM
---
+++ src/main/kotlin/org/megras/data/model/MediaType.kt
@@ -0,0 +1,21 @@
+package org.megras.data.model
+
+import org.megras.data.mime.MimeType
+
+enum class MediaType(val prefix: Char, val mimeTypes: Set<MimeType>) {
+
+    TEXT('t', setOf(MimeType.CSS, MimeType.CSV, MimeType.HTML, MimeType.JS, MimeType.JSON, MimeType.TEXT)),
+    IMAGE('i', setOf(MimeType.BMP, MimeType.GIF, MimeType.JPEG_I, MimeType.PNG, MimeType.SVG, MimeType.TIFF)),
+    AUDIO('a', setOf(MimeType.AAC, MimeType.ADP, MimeType.AIF, MimeType.AU, MimeType.MPEG_A, MimeType.MIDI, MimeType.MP4_A, MimeType.OGG, MimeType.WAV, MimeType.WAX, MimeType.WMA)),
+    //TODO...
+    UNKNOWN('x', setOf(MimeType.OCTET_STREAM));
+
+    companion object {
+
+        val mimeTypeMap = values().flatMap { t ->
+            t.mimeTypes.map { it to t }
+        }.toMap()
+
+    }
+
+}
