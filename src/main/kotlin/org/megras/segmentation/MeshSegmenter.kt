56f35a9d411d41123965f5b199020e53965e7f97
JaanWilli <jan.willi@uzh.ch>
2023-04-03 14:18:57 +0200

    Extend mesh segmenter algorithm to smoothen cut edges

--- src/main/kotlin/org/megras/segmentation/MeshSegmenter.kt
+++ src/main/kotlin/org/megras/segmentation/MeshSegmenter.kt
@@ -1,7 +1,6 @@
 package org.megras.segmentation
 
-import de.javagl.obj.Obj
-import de.javagl.obj.Objs
+import de.javagl.obj.*
 
 
 object MeshSegmenter {
@@ -26,15 +25,36 @@ object MeshSegmenter {
         for (f in 0 until obj.numFaces) {
             val face = obj.getFace(f)
 
-            var keep = true
+            val faceVertices = mutableListOf<Int>()
+            val keepVertices = mutableListOf<Int>()
             for (v in 0 until face.numVertices) {
-                val vertex = obj.getVertex(face.getVertexIndex(v))
-                val isAbove = plane.a * vertex.x + plane.b * vertex.y + plane.c * vertex.z + plane.d >= 0
-                if (isAbove != plane.above) keep = false
+                faceVertices.add(face.getVertexIndex(v))
+                val v1 = obj.getVertex(face.getVertexIndex(v))
+                val v2 = obj.getVertex(face.getVertexIndex((v + 1) % face.numVertices))
+
+                val d1 = (plane.a * v1.x + plane.b * v1.y + plane.c * v1.z + plane.d).toFloat()
+                val d2 = (plane.a * v2.x + plane.b * v2.y + plane.c * v2.z + plane.d).toFloat()
+
+                if (d1 >= 0 == plane.above) {
+                    keepVertices.add(face.getVertexIndex(v))
+                }
+
+                if (d1 * d2 < 0) {
+                    val t = -d1 / (d2 - d1)
+                    val x = v1.x + t * (v2.x - v1.x)
+                    val y = v1.y + t * (v2.y - v1.y)
+                    val z = v1.z + t * (v2.z - v1.z)
+                    out.addVertex(x, y, z)
+                    keepVertices.add(out.numVertices - 1)
+                }
             }
 
-            if (keep) {
-                out.addFace(face)
+            if (keepVertices.isNotEmpty()) {
+                if (faceVertices == keepVertices) {
+                    out.addFace(face)
+                } else {
+                    out.addFace(*keepVertices.toIntArray())
+                }
             }
         }
 
