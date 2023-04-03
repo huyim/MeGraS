593b5f916a1401371a91c13718e45de509ae53fd
JaanWilli <jan.willi@uzh.ch>
2023-04-03 16:51:48 +0200

    Display more media types on about request

--- src/main/kotlin/org/megras/api/rest/handlers/AboutObjectRequestHandler.kt
+++ src/main/kotlin/org/megras/api/rest/handlers/AboutObjectRequestHandler.kt
@@ -6,6 +6,7 @@ import org.megras.api.rest.GetRequestHandler
 import org.megras.api.rest.RestErrorStatus
 import org.megras.data.fs.FileSystemObjectStore
 import org.megras.data.graph.StringValue
+import org.megras.data.mime.MimeType
 import org.megras.data.model.MediaType
 import org.megras.data.schema.MeGraS
 import org.megras.graphstore.QuadSet
@@ -34,11 +35,34 @@ class AboutObjectRequestHandler(private val quads: QuadSet, private val objectSt
           """.trimIndent()
         )
 
-        val type = quads.filterPredicate(MeGraS.MEDIA_TYPE.uri).firstOrNull()?.`object` as? StringValue
+        val mediaType = relevant.filterPredicate(MeGraS.MEDIA_TYPE.uri).firstOrNull()?.`object` as? StringValue
+        val mimeType = relevant.filterPredicate(MeGraS.MIME_TYPE.uri).firstOrNull()?.`object` as? StringValue
 
-        when(type?.value) {
+        when(mediaType?.value) {
             MediaType.IMAGE.name -> {
-                buf.append("<img src='${objectId.toPath()}' />")
+                buf.append("<img src='${objectId.toPath()}'/>")
+            }
+
+            MediaType.VIDEO.name -> {
+                buf.append("""
+                    <video controls>
+                      <source src='${objectId.toPath()}' type='${mimeType?.value}'>
+                      Your browser does not support the video tag.
+                    </video>
+                """.trimIndent())
+            }
+
+            MediaType.AUDIO.name -> {
+                buf.append("""
+                    <audio controls>
+                      <source src='${objectId.toPath()}' type='${mimeType?.value}'>
+                      Your browser does not support the video tag.
+                    </audio>
+                """.trimIndent())
+            }
+
+            MimeType.TEXT.name -> {
+                buf.append("<embed src='${objectId.toPath()}'>")
             }
 
             else -> {/* no preview */}
