d564a9113faff0bc999fd40e040bffa7ab482d6d
JaanWilli <jan.willi@uzh.ch>
2023-06-15 16:07:55 +0200

    Allow omission of id in quad api

--- src/main/kotlin/org/megras/api/rest/data/ApiQuad.kt
+++ src/main/kotlin/org/megras/api/rest/data/ApiQuad.kt
@@ -2,6 +2,6 @@ package org.megras.api.rest.data
 
 import org.megras.data.graph.Quad
 
-data class ApiQuad(val id: String, val s: String, val p: String, val o: String) {
+data class ApiQuad(val id: String?, val s: String, val p: String, val o: String) {
     constructor(quad: Quad) : this(quad.id.toString(), quad.subject.toString(), quad.predicate.toString(), quad.`object`.toString())
 }
