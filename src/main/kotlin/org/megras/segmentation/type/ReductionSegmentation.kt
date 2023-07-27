1c93e875010978ad43cf83825fafec48422d81f9
JaanWilli <jan.willi@uzh.ch>
2023-07-27 18:29:32 +0200

    Rename ReductionalSegmentation to reflect the changes in the thesis

--- src/main/kotlin/org/megras/segmentation/type/ReductionSegmentation.kt
+++ src/main/kotlin/org/megras/segmentation/type/ReductionSegmentation.kt
@@ -3,33 +3,33 @@ package org.megras.segmentation.type
 import org.megras.segmentation.Bounds
 import org.megras.segmentation.SegmentationType
 
-abstract class ReductionalSegmentation(val selection: List<String>) : Segmentation {
+abstract class ReductionSegmentation(val selection: List<String>) : Segmentation {
     override var bounds: Bounds = Bounds()
 
     override fun equivalentTo(rhs: Segmentation): Boolean {
-        if (rhs !is ReductionalSegmentation) return false
+        if (rhs !is ReductionSegmentation) return false
         return this.contains(rhs) && rhs.contains(this)
     }
 
     override fun contains(rhs: Segmentation): Boolean {
-        if (rhs !is ReductionalSegmentation) return false
+        if (rhs !is ReductionSegmentation) return false
         return rhs.selection.all { this.selection.contains(it) }
     }
 
     override fun contains(rhs: Bounds): Boolean = false
 
     override fun orthogonalTo(rhs: Segmentation): Boolean {
-        return rhs !is ReductionalSegmentation
+        return rhs !is ReductionSegmentation
     }
 }
 
-class StreamChannel(selection: List<String>) : ReductionalSegmentation(selection) {
+class StreamChannel(selection: List<String>) : ReductionSegmentation(selection) {
     override val segmentationType: SegmentationType = SegmentationType.CHANNEL
 
     override fun getDefinition(): String = selection.joinToString(",")
 }
 
-class ColorChannel(selection: List<String>) : ReductionalSegmentation(selection) {
+class ColorChannel(selection: List<String>) : ReductionSegmentation(selection) {
     override val segmentationType: SegmentationType = SegmentationType.COLOR
 
     override fun getDefinition(): String = selection.joinToString(",")
